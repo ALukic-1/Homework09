34f89e4332eedb97c17cf1e9c038a65a63003585
alukic <alukic@smail.uni-koeln.de>
2025-06-26 17:18:01 +0200

    h3 finished

--- src/Main.java
+++ src/Main.java
@@ -14,24 +14,34 @@ public class Main {
 
         //Ex.02)
 
+        Student student = new Student("Jonas", 3, "CS");
+        Student student1 = new Student("Max", 3, "Bio");
 
+
+        Student.StudentFormatter studentFormatter = s -> "Name " + student.name + " Year " + student.year;
+        Student.StudentFormatter studentFormatter1 = s -> student.name + " studies " + student.major + " in Year " + student.year;
+
+
+        System.out.println(studentFormatter.format(student));
+        System.out.println(studentFormatter1.format(student));
+        System.out.println(studentFormatter.format(student1));
+        System.out.println(studentFormatter1.format(student1));
 
 
 
         //Ex.03)
         Movie movie = new Movie("Shrek", 9.5, 2000);
-        Movie movie1 = new Movie("Cars", 8.5,2002);
+        Movie movie1 = new Movie("Cars", 8.5, 2002);
         Movie movie2 = new Movie("Mulan", 7, 2010);
         Movie movie3 = new Movie("Bambi", 10, 2004);
         Movie movie4 = new Movie("Rapunzel", 7.5, 2015);
 
 
-
-        Movie [] movies = {movie, movie1, movie2, movie3, movie4};
+        Movie[] movies = {movie, movie1, movie2, movie3, movie4};
 
         Arrays.stream(movies)
                 .filter(m -> m.rating >= 8.5)
-                .sorted(Comparator.comparing(Movie :: getYear ).thenComparing(Movie:: getRating).reversed())
+                .sorted(Comparator.comparing(Movie::getYear).thenComparing(Movie::getRating).reversed())
                 .forEach(m -> System.out.println(m.getTitle() + " - " + m.getRating()));
 
     }
--- src/Student.java
+++ src/Student.java
@@ -2,4 +2,18 @@ public class Student {
     String name;
     int year;
     String major;
+
+
+    public Student(String name, int year, String major) {
+        this.name = name;
+        this.year = year;
+        this.major = major;
+    }
+
+
+
+    public interface StudentFormatter { String format(Student s); }
+
+
 }
+
