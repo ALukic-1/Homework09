8a05dc4921fc7160d35795b778ad4f9538df0a14
alukic <alukic@smail.uni-koeln.de>
2025-06-26 14:24:22 +0200

    h1

--- src/CoursePhase.java
+++ src/CoursePhase.java
@@ -1,3 +1,17 @@
 public enum CoursePhase {
-    NOT_STARTED, ONGOING, FINISHED
+    NOT_STARTED ("Course not started yet"),
+    ONGOING ("Course is in progress"),
+    FINISHED ("Course has ended");
+
+
+    private String Message;
+
+     CoursePhase (String Message){
+        this.Message = Message;
+
+    }
+
+    public String getMessage() {
+        return Message;
+    }
 }
